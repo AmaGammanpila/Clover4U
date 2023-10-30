069e76d3d938455bbda241dd7341fd8991a68d13
AmaGammanpila <145711182+AmaGammanpila@users.noreply.github.com>
2023-10-30 12:39:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("button").onclick = function () {
+    location.href = "contactus.html";
+};
+
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
+
+navToggle.addEventListener('click', function(){
+    links.classList.toggle("show_nav");
+})
+
+document.getElementById("button2").onclick = function () {
+    location.href = "contactus.html";
+};
+document.getElementById("button3").onclick = function () {
+    location.href = "contactus.html";
+};
+document.getElementById("button4").onclick = function () {
+    location.href = "contactus.html";
+};
+document.getElementById("button5").onclick = function () {
+    location.href = "contactus.html";
+};
+document.getElementById("button6").onclick = function () {
+    location.href = "contactus.html";
+};
